5e26d5f4b2da3ddc57d615a4c00c148e594164cf
Gabriel D <gabrieldavidfs@hotmail.com>
2023-02-17 20:53:47 +0100

    Added test for localStorage, functional

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css"
 
 function Login() {
+
 
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
@@ -14,12 +15,22 @@ function Login() {
         setPassword(event.target.value);
     }
 
+    function saveData (event){
+        localStorage.setItem ("nameKey", JSON.stringify(userName))
+        console.log(userName)
+        
+        const retrievedData = JSON.parse (localStorage.getItem("nameKey"))
+        console.log(retrievedData)
+        
+        
+    }
+
     return (
     <>
         <div className={styles.login}>
             <input type="text" name="userName" placeholder="Nombre de Usuario" onInput={handlerUserName}></input>
             <input type="password" name="password" placeholder="Contraseña" onInput={handlerPassword}></input>
-            <button>Inicia sesión</button>
+            <button onClick={saveData}>Inicia sesión</button>
         </div>
     </>
     );
